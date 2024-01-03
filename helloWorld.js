2cdfdc2bf90f74876c7d1962ad7b568d66138c91
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2024-01-03 12:45:31 +0530

    Day 1

---
+++ helloWorld.js
@@ -0,0 +1,17 @@
+// 2667. Create Hello World Function
+//tc O(1)
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World";
+        
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */

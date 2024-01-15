14616b9425c62f54b44a1c2e95edaba2639edb13
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2024-01-15 23:44:23 +0530

    Day 10

---
+++ CallOnce.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let toggle = true;
+    
+    return function(...args){
+        if(toggle){
+            toggle = false;
+            return fn(...args);
+        }
+        
+    }
+};
+
+let fn = (a,b,c) => (a + b + c)
+let onceFn = once(fn)
+console.log(onceFn(1,2,3)); // 6
+console.log(onceFn(2,3,6)); // returns undefined without calling fn
+

94bc6dd3e63ccf43dad4f61dfb68548126a4aae4
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2024-02-01 19:18:22 +0530

    Day 26

---
+++ CompactObject.js
@@ -0,0 +1,26 @@
+/**
+ * @param {Object|Array} obj
+ * @return {Object|Array}
+ */
+var compactObject = function(obj) {
+    if (typeof obj === 'object') {
+        if (Array.isArray(obj)) {
+            const tempArray = [];
+            for (let index = 0; index < obj.length; index++) {
+                if (Boolean(obj[index])) {
+                    tempArray.push(compactObject(obj[index]));
+                }
+            }
+            return tempArray;
+        } else {
+            const tempObject = {};
+            for (const key in obj) {
+                if (Boolean(obj[key])) {
+                    tempObject[key] = compactObject(obj[key]);
+                }
+            }
+            return tempObject;
+        }
+    }
+    return obj;
+};
